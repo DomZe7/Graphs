150c4d430fea87a5872eb2ef562a3ac2c1e1c04b
Dominic Zygadlo <dzygadlo@asu.edu>
2020-10-29 01:09:00 -0700

    lolidone

--- lollipop.py
+++ lollipop.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
-import plotnine as plt
+from plotnine import *
+from plotnine.data import *
+
+path = r'C:\Users\domin\OneDrive\Desktop\Graphs\Figures\Lollipops'
 
 d = {
     'subject': ['a', 'b', 'c', 'd'],
@@ -11,3 +14,9 @@ d = {
 }
 
 df = pd.DataFrame(data=d)
+df.sort_values(by=['maximal'], inplace=True)
+        
+gplot = ggplot(aes(x='reorder(subject, maximal)', y='maximal', color='response', label='maximal'), df) + geom_segment(aes(x='subject', xend='subject', y=0, yend='maximal'), color='grey')+ geom_point(size=8)+scale_y_continuous(labels=lambda l: ["%d%%" % (v * 100) for v in l])+ geom_text(color='black', size=6)
+color = scale_color_manual(values=['#f52525', '#59db2a', '#dbc62a'])
+plot = gplot+color+xlab('Subject')+ylab('Percent Change')+ggtitle('Best Percent Changes in Maximal')
+plot.save(filename='tutorial.png', path=path)
